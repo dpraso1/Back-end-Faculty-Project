effc0ebcb70bb6cf635c4adefadafb05896c5f63
Dajana Prašo <dpraso1@etf.unsa.ba>
2022-11-11 22:17:41 +0100

    Ispravke + testovi

--- prisustvo.js
+++ prisustvo.js
@@ -1,9 +1,9 @@
 class Prisustvo {
 
-    static trenutnaSedmica;
+    static trenutnaSedmica = 1;
 
     constructor() {
-        this.trenutnaSedmica = 1;
+
         this.prisustvo = 0;
         this.finalnoStanje = false;
     }
@@ -11,45 +11,20 @@ class Prisustvo {
     izracunajPrisustvo(sedmica, listaPrisustva) {
 
 
-        let rez = { greska: ""};
+        let rez = { greska: "" };
+        // var LP = listaPrisustva;
 
         if (sedmica < 1 || sedmica > 15 || isNaN(sedmica)) {
             rez.greska = "Parametar sedmica nema vrijednost u rasponu od 1 do 15!";
             return rez;
-        } else if (sedmica >= this.trenutnaSedmica) {
+        }
+        if (sedmica >= Prisustvo.trenutnaSedmica) {
             rez.greska = "Parametar sedmica mora imati vrijednost koja je manja od trenutne sedmice!";
             return rez;
         }
 
-       
-        let ukupno_prisutan = 0;
-        let ukupno_odsutan = 0;
-        
+
         for (let i = 0; i < listaPrisustva.length; i++) {
-
-            const dupleVrijednosti = listaPrisustva
-                .map(v => v.prSedmica)
-                .filter((v, i, vprS) => vprS.indexOf(v) !== i)
-            
-            //console.log(dupleVrijednosti.length);
-            const listaDuplih = listaPrisustva
-                .filter(obj => dupleVrijednosti.includes(obj.prSedmica));
-            
-           
-            
-            // console.log(listaDuplih.length)
-           
-                
-               let zadnji = listaDuplih.pop();
-                
-            
-            //console.log(zadnji)
-
-            let greska_prSedmica = -1;
-            let greska_prisutan = -1;
-            let greska_odsutan = -1;
-            let greska_nijeUneseno = -1;
-
 
             if (!listaPrisustva[i].hasOwnProperty('prSedmica') ||
                 !listaPrisustva[i].hasOwnProperty('prisutan') ||
@@ -59,6 +34,14 @@ class Prisustvo {
                 return rez;
 
             }
+        }
+        let greska_prSedmica = -1;
+        let greska_prisutan = -1;
+        let greska_odsutan = -1;
+        let greska_nijeUneseno = -1;
+
+        for (let i = 0; i < listaPrisustva.length; i++) {
+
             if (listaPrisustva[i].prSedmica < 1 || listaPrisustva[i].prSedmica > 15) {
                 greska_prSedmica = i;
             }
@@ -75,20 +58,20 @@ class Prisustvo {
             if (greska_prSedmica != -1 || greska_prisutan != -1 || greska_odsutan != -1 || greska_nijeUneseno != -1) {
 
                 rez.greska = "Parametar listaPrisustva nema ispravne vrijednosti za sedmicu " + listaPrisustva[i].prSedmica + " za properties [";
-                
+
                 if (greska_prisutan != -1)
                     rez.greska += "prisutan";
-                
+
                 if (greska_odsutan != -1) {
                     if (greska_prisutan != -1)
                         rez.greska += ",odsutan";
                     else rez.greska += "odsutan"
                 }
-            
+
                 if (greska_nijeUneseno != -1) {
                     if (greska_prisutan != -1 || greska_odsutan != -1)
                         rez.greska += ",nijeUneseno";
-                    
+
                     else rez.greska += "nijeUneseno";
                 }
 
@@ -96,78 +79,41 @@ class Prisustvo {
                 return rez;
 
             }
-        
-            
+        }
+
+        for (let i = 0; i < listaPrisustva.length; i++) {
             if (listaPrisustva[i].prisutan + listaPrisustva[i].odsutan + listaPrisustva[i].nijeUneseno > 8) {
                 rez.greska = "Parametar listaPrisustva nema ispravnu zbirnu vrijednost!"
                 return rez;
             }
+        }
 
-
+        let ukupno_prisutan = 0;
+        let ukupno_odsutan = 0;
+        for (let i = 0; i < listaPrisustva.length; i++) {
             ukupno_prisutan += listaPrisustva[i].prisutan;
             ukupno_odsutan += listaPrisustva[i].odsutan;
-            console.log(ukupno_prisutan)
-            console.log(ukupno_odsutan)
-            this.prisustvo = (ukupno_prisutan) / (ukupno_prisutan + ukupno_odsutan) * 100;
-            let prisustvo = this.prisustvo.toFixed(2);
-            console.log(prisustvo);
-
-           /* if (listaPrisustva[i].nijeUneseno === 0) this.finalnoStanje;
-            console.log(this.finalnoStanje)
-
-           /* let obj = {
-                prisistvoZaSedmicu:"",
-                prisutan: "",
-                odsutan: "",
-                nijeUneseno: ""
-            }
-
-            let obj = Object.assign({}, listaPrisustva[i]);
-          
-
-            obj.prSedmica = sedmica;
-            obj.prisistvoZaSedmicu=obj.prSedmica
-            delete obj.prSedmica;
-            console.log(obj)
-            if(obj.prisistvoZaSedmicu == listaPrisustva[i].prSedmica)
-            {
-                
-            }
-         
-           */
-
         }
-        
-        return console.log("nesto")
 
+        console.log(ukupno_prisutan)
+        console.log(ukupno_odsutan)
+        this.prisustvo = (ukupno_prisutan) / (ukupno_prisutan + ukupno_odsutan) * 100;
+        let prisustvo = this.prisustvo.toFixed(2);
+        console.log(prisustvo);
 
+        let provjeri = true;
 
-}
+        for (let i = 0; i < listaPrisustva.length; i++) {
+            if (listaPrisustva[i].nijeUneseno != 0)
+                provjeri = false;
+        } if (provjeri) {
+            this.finalnoStanje = true;
+        } else {
+            this.finalnoStanje = false;
+        }
+
+        console.log(this.finalnoStanje)
+
+    }
 
 };
-
-
-let proba = new Prisustvo();
-let sedmica = 1;
-let lp = `{
-    [{
-        "prSedmica": "1",
-        "prisutan": "2",
-        "odsutan": "0",
-        "nijeUneseno": "0"
-    },
-    {
-        "prSedmica": "1",
-        "prisutan": "0",
-        "odsutan": "0",
-        "nijeUneseno": "0"
-    },
-    {
-        "prSedmica": "1",
-        "prisutan": "0",
-        "odsutan": "0",
-        "nijeUneseno": "0"
-    }];
-}`;
-
-console.log(proba.izracunajPrisustvo(sedmica, lp));
--- test.js
+++ test.js
@@ -2,7 +2,7 @@ let assert = chai.assert;
 describe('Prisustvo', function () {
     describe('#izracunajPrisustvo()', function () {
         //TEST 1
-        it('parametar sedmica nema vrijednost u rasponu od 1 do 15 (uključujući 1 i 15)', function () {
+        it('parametar sedmica nema vrijednost u rasponu od 1 do 15 (uključujući 1 i 15) - negativna vrijednost', function () {
             let proba1 = new Prisustvo();
             let sedmica1 = -1;
             let lp1 = `{
@@ -14,24 +14,127 @@ describe('Prisustvo', function () {
 
             assert.deepEqual(proba1.izracunajPrisustvo(sedmica1, lp1), g1);
         });
-        //TEST2
-        it('parametar ima vrijednost koja je veća od vrijednosti atributa trenutnaSedmica', function () {
+        it('parametar sedmica nema vrijednost u rasponu od 1 do 15 (uključujući 1 i 15) - decimalna vrijednost', function () {
             let proba1 = new Prisustvo();
-            let sedmica1 = 3;
+            let sedmica1 = 0.5;
             let lp1 = `{
                 [{ prSedmica: 1, prisutan: 5, odsutan: 2, nijeUneseno: 0 }, 
                  { prSedmica: 1, prisutan: 2, odsutan: 2, nijeUneseno: 1 }, 
+                 { prSedmica: 1, prisutan: 1, odsutan: 1, nijeUneseno: 1 }];
+            }`;
+            let g1 = { greska: "Parametar sedmica nema vrijednost u rasponu od 1 do 15!" };
+
+            assert.deepEqual(proba1.izracunajPrisustvo(sedmica1, lp1), g1);
+        });
+        it('parametar sedmica nema vrijednost u rasponu od 1 do 15 (uključujući 1 i 15) - vrijednost veća od 15', function () {
+            let proba1 = new Prisustvo();
+            let sedmica1 = 16;
+            let lp1 = `{
+                [{ prSedmica: 1, prisutan: 5, odsutan: 2, nijeUneseno: 1}, 
+                 { prSedmica: 1, prisutan: 2, odsutan: 2, nijeUneseno: 1 }, 
+                 { prSedmica: 1, prisutan: 1, odsutan: 1, nijeUneseno: 1 }];
+            }`;
+            let g1 = { greska: "Parametar sedmica nema vrijednost u rasponu od 1 do 15!" };
+
+            assert.deepEqual(proba1.izracunajPrisustvo(sedmica1, lp1), g1);
+        });
+        //TEST2
+        it('parametar sedmica ima vrijednost koja je veća od vrijednosti atributa trenutnaSedmica', function () {
+            let proba1 = new Prisustvo();
+            Prisustvo.trenutnaSedmica = 1;
+            let sedmica1 = 3;
+            let lp1 = `{
+                [{ prSedmica: 1, prisutan: 1, odsutan: 2, nijeUneseno: 1 }, 
+                 { prSedmica: 1, prisutan: 2, odsutan: 1, nijeUneseno: 1 }, 
                  { prSedmica: 1, prisutan: 1, odsutan: 1, nijeUneseno: 1 }];
             }`;
             let g1 = { greska: "Parametar sedmica mora imati vrijednost koja je manja od trenutne sedmice!" };
 
             assert.deepEqual(proba1.izracunajPrisustvo(sedmica1, lp1), g1);
         });
-       
+        //TEST3
+        it('objekat ili objekti parametra listaPrisustva ima/imaju neispravan jedan ili više properties (npr. nemaju property prisustvo)', function () {
+            let proba1 = new Prisustvo();
+            Prisustvo.trenutnaSedmica = 2;
+            let sedmica1 = 1;
+            let lp1 = `{
+                [{ prSedmica: 1, , odsutan: 2, nijeUneseno: 0 }, 
+                 { prSedmica: 1, prisutan: 2, odsutan: 2, nijeUneseno: 1 }, 
+                 { prSedmica: 1, prisutan: 1, odsutan: 1, nijeUneseno: 1 }];
+            }`;
+            let g1 = { greska: "Parametar listaPrisustva nema ispravne properties!" };
+
+            assert.deepEqual(proba1.izracunajPrisustvo(sedmica1, lp1), g1);
+        });
+        it('objekat ili objekti parametra listaPrisustva ima/imaju neispravan jedan ili više properties (npr. nemaju property prisustvo)', function () {
+            let proba1 = new Prisustvo();
+            let sedmica1 = 1;
+            let lp1 = `{
+                [{ prSedmica: 1, prisutan: 1, odsutan: 2, nijeUneseno: -10 }, 
+                 { prSedmica: 1, prisutan: 2, odsutan: 2, nijeUneseno: 1 }, 
+                 { prSedmica: 1, prisutan: 1, odsutan: 1, nijeUneseno: 1 }];
+            }`;
+            let g1 = { greska: "Parametar listaPrisustva nema ispravne properties!" };
+
+            assert.deepEqual(proba1.izracunajPrisustvo(sedmica1, lp1), g1);
+        });
+        //TEST4
+        /*it('zbir properties prisutan, odsutan i nijeUneseno za jednu ili više sedmica je veći od 8)', function () {
+            let proba1 = new Prisustvo();
+            Prisustvo.trenutnaSedmica = 1;
+            let sedmica1 = 1;
+            let lp1 = `{
+                [{ prSedmica: 2, prisutan: 1, odsutan: 2, nijeUneseno: 2 }, 
+                 { prSedmica: 1, prisutan: 2, odsutan: 2, nijeUneseno: 1 }, 
+                 { prSedmica: 1, prisutan: 7, odsutan: 1, nijeUneseno: 1 }];
+            }`;
+            let g1 = { greska: "Parametar listaPrisustva nema ispravnu zbirnu vrijednost!" };
+
+            assert.deepEqual(proba1.izracunajPrisustvo(sedmica1, lp1));
+        });
+        //TEST5
+        it('parametri sedmica i listaPrisustva ne ispunjavaju više uslova', function () {
+            let proba1 = new Prisustvo();
+            let sedmica1 = 1;
+            let lp1 = `{
+                [{ prSedmica: 1, prisutan: 6, odsutan: 2, nijeUneseno: 1 }, 
+                 { prSedmica: 1, prisutan: 2, odsutan: 2, nijeUneseno: 1 }, 
+                 { prSedmica: 1, prisutan: 1, odsutan: 7, nijeUneseno: 1 }];
+            }`;
+            let g1 = { greska: "Parametar listaPrisustva nema ispravnu zbirnu vrijednost!" };
+
+            assert.deepEqual(proba1.izracunajPrisustvo(sedmica1, lp1), g1);
+        });
+        //TEST6
+        it('parametri su ispravni i metoda vraća objekat sa prisustvom za sedmicu koja je navedena kao parametar sedmica', function () {
+            let proba1 = new Prisustvo();
+            let sedmica1 = 2;
+            let lp1 = `{
+                [{ prSedmica: 1, prisutan: 6, odsutan: 2, nijeUneseno: 1 }, 
+                 { prSedmica: 1, prisutan: 2, odsutan: 2, nijeUneseno: 1 }, 
+                 { prSedmica: 1, prisutan: 1, odsutan: 7, nijeUneseno: 1 }];
+            }`;
+            let g1 = { greska: "Parametar listaPrisustva nema ispravnu zbirnu vrijednost!" };
+
+            assert.deepEqual(proba1.izracunajPrisustvo(sedmica1, lp1), g1);
+        });
+        */
+        //TEST7
+        it('metoda ispravno računa vrijednost atributa prisustvo', function () {
+            let proba1 = new Prisustvo();
+            Prisustvo.trenutnaSedmica = 2;
+            let sedmica1 = 1;
+            let lp1 = `{
+                [{ prSedmica: 1, prisutan: 1, odsutan: 2, nijeUneseno: 1 }, 
+                 { prSedmica: 1, prisutan: 2, odsutan: 1, nijeUneseno: 1 }, 
+                 { prSedmica: 1, prisutan: 1, odsutan: 1, nijeUneseno: 1 }];
+            }`;
+            let prisutan = 4;
+            let odsutan = 4;
+            proba1.prisustvo = (prisutan)/(prisutan+odsutan) * 100;
+
+            assert.deepEqual(proba1.prisustvo, 50);
+        });
 
     });
 });
-
-
-
-
